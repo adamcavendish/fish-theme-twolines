1441840f5e91bd2e5e3105c46ee0ef0622a8a9c5
Adam Cavendish <GetbetterABC@yeah.net>
2016-10-21 11:28:21 +0800

    Move username and host to left

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -8,14 +8,29 @@ function fish_prompt --description 'Write out the prompt'
 
   __terlar_git_prompt
   __fish_hg_prompt
-  echo
+
 
   if not test $last_status -eq 0
+    # if error
     set_color $fish_color_error
-  else
-    set_color purple
-  end
+    echo -n ' '$last_status' ↵'
 
-  echo -n '➤ '
+    echo
+
+    echo -n '➤ '
+  else
+    # User & Hostname
+    set_color $fish_color_user
+    echo -n ' '(whoami)
+    set_color $fish_color_host
+    echo -n '@'(hostname -s)
+
+    echo
+
+    set_color purple
+    echo -n '➤ '
+  end
   set_color normal
+
+  echo
 end
